7beb14470c76363ca8dd8f831f01cc6378fab8d1
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-03-06 01:47:19 +0200

    Initial commit

---
+++ Day3_WPF_ITI/Ball.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Day3_WPF_ITI
+{
+    /// <summary>
+    /// Interaction logic for Ball.xaml
+    /// </summary>
+    public partial class Ball : Window
+    {
+        public Ball()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 200;
+            animation.To = 500;
+            animation.Duration = TimeSpan.FromSeconds(2);
+            animation.RepeatBehavior = RepeatBehavior.Forever;
+            animation.AutoReverse = true;
+            ball.BeginAnimation(Canvas.LeftProperty, animation);
+
+        }
+
+    }
+}
